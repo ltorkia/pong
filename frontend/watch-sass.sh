c29cc4d6857adeffa07c56e742c2894b261b670f
Lee <lee.torkia@gmail.com>
2025-05-17 21:25:22 +0200

    tree

---
+++ frontend/watch-sass.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Sass compiler location
+SASS_EXEC="./assets/styles/scss/dart-sass/sass"
+
+# Source and destination files
+SRC="./assets/styles/scss/index.scss"
+DEST="./assets/styles/css/main.css"
+
+# Check if compiler is found
+if [ ! -f "$SASS_EXEC" ]; then
+  echo "❌ Error : Sass compiler not found at $SASS_EXEC"
+  echo "➡️  Make sure Dart Sass is in the right folder."
+  exit 1
+fi
+
+# Start watching
+echo "✅ Watch Sass started. Compiling from $SRC to $DEST"
+$SASS_EXEC "$SRC" "$DEST" --watch
