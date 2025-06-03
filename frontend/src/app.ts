64cdfa98eaaefd9a3310d62e051be374ad1fb031
Lee <lee.torkia@gmail.com>
2025-06-03 17:53:30 +0200

    typage app.ts

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -4,7 +4,7 @@ import { AppManager } from './managers/AppManager';
 /**
  * Point d'entrée principal de l'app.
  */
-document.addEventListener('DOMContentLoaded', async () => {
-	const appManager = new AppManager();
+document.addEventListener('DOMContentLoaded', async (): Promise<void> => {
+	const appManager: AppManager = new AppManager();
 	await appManager.start();
 });
