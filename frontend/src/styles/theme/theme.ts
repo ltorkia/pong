42149c55dd994a82fcda3ed91b1f8ff9fa2a3477
Lee <lee.torkia@gmail.com>
2025-06-03 22:50:34 +0200

    theme.ts cleaned

--- frontend/src/styles/theme/theme.ts
+++ frontend/src/styles/theme/theme.ts
@@ -1,47 +1,47 @@
 export const fonts = {
-  arial: ['Arial', 'sans-serif'],
-  cinzel: ['"Cinzel Decorative"', 'serif'],
-  garamond: ['"Cormorant Garamond"', 'serif'],
+	arial: ['Arial', 'sans-serif'],
+	cinzel: ['"Cinzel Decorative"', 'serif'],
+	garamond: ['"Cormorant Garamond"', 'serif'],
 } as const;
 
 export const colorsTheme1 = {
-  vdarkBlue: '#10131B',
-  darkBlue: '#101626',
-  darkForestGreen: '#0E191B',
-  fadeForestGreen: '#1A2B2E',
-  forestGreen: '#102D2C',
-  lightForestGreen: '#275857',
-  vlightForestGreen: '#3C5A5E',
-  greyishGreen: '#728D8F',
-  pureMint: '#97D0BE',
-  mint: '#8AC8CD',
-  vclearSky: '#87CEEB',
-  deepSky: '#12C5C2',
-  deepCyan: '#3762D5',
-  deepPurple: '#4A2F6B',
-  lilac: '#B38BFF',
-  vlightLilac: '#C7B8E4',
-  orangyYellow: '#F09400',
-  fireflyYellow: '#FFD37C',
-  grey: '#777381',
-  vlightGrey: '#ABBBBD',
-  white: '#FFFFFF',
-  black: '#000000'
+	vdarkBlue: '#10131B',
+	darkBlue: '#101626',
+	darkForestGreen: '#0E191B',
+	fadeForestGreen: '#1A2B2E',
+	forestGreen: '#102D2C',
+	lightForestGreen: '#275857',
+	vlightForestGreen: '#3C5A5E',
+	greyishGreen: '#728D8F',
+	pureMint: '#97D0BE',
+	mint: '#8AC8CD',
+	vclearSky: '#87CEEB',
+	deepSky: '#12C5C2',
+	deepCyan: '#3762D5',
+	deepPurple: '#4A2F6B',
+	lilac: '#B38BFF',
+	vlightLilac: '#C7B8E4',
+	orangyYellow: '#F09400',
+	fireflyYellow: '#FFD37C',
+	grey: '#777381',
+	vlightGrey: '#ABBBBD',
+	white: '#FFFFFF',
+	black: '#000000'
 } as const;
 
 export const particleColors = {
-  forestGold: '#FFD700',
-  forestLight: '#FFF8DC',
-  forestLavender: '#E6E6FA',
-  forestSky: '#87CEEB',
-  forestGreen: '#98FB98',
-  forestYellow: '#F0E68C',
-  forestFirefly: '#FFFF00'
+	forestGold: '#FFD700',
+	forestLight: '#FFF8DC',
+	forestLavender: '#E6E6FA',
+	forestSky: '#87CEEB',
+	forestGreen: '#98FB98',
+	forestYellow: '#F0E68C',
+	forestFirefly: '#FFFF00'
 } as const;
 
 export const images = {
-  forestWallpaper: "url('/img/design/forest-bg.jpg')",
-  avatarDefault: "url('/img/avatars/elisa.jpg')",
+	forestWallpaper: "url('/img/design/forest-bg.jpg')",
+	avatarDefault: "url('/img/avatars/elisa.jpg')",
 } as const;
 
 /**
