10eca6dc1bcb5969b4e9f76892d349550630c4a9
Kilian Pouliquen <kipouliq@f2r9s14.clusters.42paris.fr>
2025-07-07 17:30:32 +0200

    working on collisions

--- frontend/src/pages/game.page.ts
+++ frontend/src/pages/game.page.ts
@@ -43,6 +43,7 @@ import { RouteConfig } from '../types/routes.types';
  * 		- mettre à jour la liste des composants de GamePage dans la config routes.config.ts
  * 		- intégrer le composant à ta page...
  */
+
 export class GamePage extends BasePage {
 	/**
 	 * GamePage hérite de BasePage (frontend/src/pages/base.page.ts) qui:
@@ -53,14 +54,68 @@ export class GamePage extends BasePage {
 	 * ! Voir les propriétés + méthodes de surcharge qui peuvent être utilisées ici.
 	 */
 
+	static PlayerBar = class {
+		x: number;
+		y: number;
+		w: number;
+		h: number;
+		ctx: CanvasRenderingContext2D;
+		draw() {
+			this.ctx.fillStyle = "rgba(255, 0, 0)";
+			this.ctx.fillRect(this.x, this.y - (this.h / 2), this.w, this.h);
+			this.ctx.fillStyle = "rgba(0, 255, 0)";
+			this.ctx.fillRect(this.x, this.y, 1, 1);
+		};
+		move(newY: number) {
+			if (newY > 0 && this.y + newY + (this.h / 2) < this.ctx.canvas.clientHeight
+			|| newY < 0 && this.y + newY - (this.h / 2) > 0)
+				this.y += newY;
+		}
+		constructor(ctx: CanvasRenderingContext2D) {
+			this.x = 0;
+			this.y = 0;
+			this.w = 20;
+			this.h = 100;
+			this.ctx = ctx;
+		}
+	}
+
+	static Ball = class {
+		x: number;
+		y: number;
+		vx: number;
+		vy: number;
+		radius: number;
+		ctx: CanvasRenderingContext2D;
+		draw() {
+			this.ctx.beginPath();
+			this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+			this.ctx.closePath();
+			this.ctx.fillStyle = "rgba(0, 0, 255)";
+			this.ctx.fill();
+		};
+		constructor(ctx: CanvasRenderingContext2D) {
+			this.x = 0;
+			this.y = 0;
+			this.vx = 5;
+			this.vy = 2;
+			this.radius = 10;
+			this.ctx = ctx;
+		}
+	}
+
     private gameCanvas: HTMLCanvasElement = document.createElement('canvas');
-    private canvasCtx: CanvasRenderingContext2D = this.gameCanvas.getContext("2d")!;
-    private playerOnePosY: number = 0;
+    private canvasCtx: CanvasRenderingContext2D = this.gameCanvas.getContext("2d", {alpha: true})!;
+	private playerOne = new GamePage.PlayerBar(this.canvasCtx);
+	private playerTwo = new GamePage.PlayerBar(this.canvasCtx);
+	private ball = new GamePage.Ball(this.canvasCtx);
+	private frameReq: number;
 
 	constructor(config: RouteConfig) {
 		// super() = appelle le constructeur du parent BasePage et lui transmet la config de la page Game.
 		// avec le container et le chemin du template HTML pour injecter la page.
 		super(config);
+		// this.frameReq = 0;
 	}
 	
 	// méthode mount() = Ce qu'on injecte dynamiquement dans la page après avoir injecté le template HTML dans la div #app.
@@ -69,33 +124,54 @@ export class GamePage extends BasePage {
 	protected async mount(): Promise<void> {
 		this.initGame();
 	}
-	
+
 	// Les potentiels events de la page ?
 	protected attachListeners(): void {
-        const canvasContainer: HTMLElement = document.getElementById("pong-section")!;
 		document.addEventListener("keydown", (event) => {});
 		onkeydown = (event) => {
 			console.log(event);
 			if (event.key == "w")
-				this.playerOnePosY -= 10;
+				this.playerOne.move(-10);
 			else if (event.key == "s")
-				this.playerOnePosY += 10;
-            // console.log(this.playerOnePosY);
+				this.playerOne.move(10);
+			if (event.key == "ArrowUp")
+				this.playerTwo.move(-10);
+			else if (event.key == "ArrowDown")
+				this.playerTwo.move(10);
 		}
 	}
 
     private clearScreen(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement): void {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+		this.canvasCtx.globalCompositeOperation = 'destination-out';
+		this.canvasCtx.fillStyle = "rgba(0, 0, 0, 0.3)";
+        this.canvasCtx.fillRect(0, 0, this.gameCanvas.width, this.gameCanvas.height);
+		this.canvasCtx.globalCompositeOperation = 'source-over';
     }
+
+	private checkCollision(): void {
+		if (this.ball.x < 0 && this.ball.x <= this.playerOne.x + this.playerOne.w || this.ball.x >= this.playerTwo.x) {
+			if (this.ball.y > this.playerOne.y + this.playerOne.h / 2
+				|| this.ball.y < this.playerOne.y - this.playerOne.h / 2) {
+					console.log("cancel !");
+					cancelAnimationFrame(this.frameReq);
+					return;
+				}
+			else
+				this.ball.vx *= -1;
+		}
+		if (this.ball.y <= 0 || this.ball.y >= this.canvasCtx.canvas.height)
+				this.ball.vy *= -1;
+	}
 	
 	private gameLoop = () => {
-        // this.clearScreen(this.canvasCtx, this.gameCanvas);
-        this.canvasCtx.fillStyle = 'rgba(255, 255, 255, 0.1)';
-        this.canvasCtx.fillRect(0, 0, this.gameCanvas.width, this.gameCanvas.height);
-        this.canvasCtx.filter = 'blur(1.5px)';
-		this.canvasCtx.fillStyle = "rgb(255, 0, 0)";
-        this.canvasCtx.fillRect(0, this.playerOnePosY, 20, 100);
-        this.canvasCtx.filter = 'none';
+        this.clearScreen(this.canvasCtx, this.gameCanvas);
+		this.playerOne.draw();
+		this.playerTwo.draw();
+		this.checkCollision();
+		this.ball.x += this.ball.vx;
+		this.ball.y += this.ball.vy;
+		this.ball.draw();
+		this.canvasCtx.filter = 'none';
         requestAnimationFrame(this.gameLoop);
 	}
 
@@ -104,8 +180,13 @@ export class GamePage extends BasePage {
         canvasContainer.style.border = "1px solid black";
 		this.gameCanvas.height = canvasContainer.getBoundingClientRect().height;    // will need to update that every frame later (responsiveness)
 		this.gameCanvas.width = canvasContainer.getBoundingClientRect().width;
+		this.playerOne.y = this.playerTwo.y = this.gameCanvas.height / 2;
+		this.playerTwo.x = this.gameCanvas.width - this.playerTwo.w;
+		this.ball.x = this.gameCanvas.width / 2;
+		this.ball.y = this.gameCanvas.height / 2;
+		this.gameCanvas.style.border = "1px solid black";
         canvasContainer.append(this.gameCanvas);
-        requestAnimationFrame(this.gameLoop);
+        this.frameReq = requestAnimationFrame(this.gameLoop);
 	}
 
 	// Amuse-toi biiiiiiennnnnnn ! =D
