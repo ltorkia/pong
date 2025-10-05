530530a5176d690e2045f0f8d0f19f9c7120fe08
Lee <lee.torkia@gmail.com>
2025-10-05 16:09:22 +0200

    setting dashboard

--- frontend/src/pages/user/dashboard.page.ts
+++ frontend/src/pages/user/dashboard.page.ts
@@ -1,7 +1,13 @@
 import { BasePage } from '../base/base.page';
 import { RouteConfig } from '../../types/routes.types';
 import { getHTMLElementById } from '../../utils/dom.utils';
+import { User } from '../../shared/models/user.model';
+import { Friend } from '../../shared/models/friend.model';
+import { Game } from '../../shared/models/game.model';
+import { Tournament } from '../../shared/models/tournament.model';
+import { dataApi, friendApi } from '../../api/index.api';
 import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, ArcElement, DoughnutController } from 'chart.js';
+import { dataService, translateService } from '../../services/index.service';
 
 // ===========================================
 // DASHBOARD PAGE
@@ -12,15 +18,10 @@ import { Chart, LineController, LineElement, PointElement, LinearScale, Title, C
 export class DashboardPage extends BasePage {
 	private dahsboardContainer!: HTMLElement;
 
-	private totalGames: number = 0;
-	private winRate: number = 0;
-	private avgScore: number = 0;
-	private streak: number = 0;
-
-	private totalGamesElm!: HTMLElement;
-	private winRateElm!: HTMLElement;
-	private avgScoreElm!: HTMLElement;
-	private streakElm!: HTMLElement;
+	private userStats: User | null = null;
+	private userFriends: Friend[] = [];
+	private userGames: Game[] = [];
+	private userTournaments: Tournament[] = [];
 
 	private lineCtx!: HTMLCanvasElement;
 	private pieCtx!: HTMLCanvasElement;
@@ -46,45 +47,130 @@ export class DashboardPage extends BasePage {
 		Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, ArcElement, DoughnutController);
 
 		this.dahsboardContainer = getHTMLElementById('dashboard');
-		this.totalGamesElm = getHTMLElementById('total-games');
-		this.winRateElm = getHTMLElementById('win-rate');
-		this.avgScoreElm = getHTMLElementById('avg-score');
-		this.streakElm = getHTMLElementById('streak');
 
 		this.lineCtx = document.getElementById('lineChart') as HTMLCanvasElement;
 		this.pieCtx = document.getElementById('pieChart') as HTMLCanvasElement;
+
+		try {
+			this.userStats = await dataApi.getUserStats(this.currentUser!.id);
+			this.userFriends = await friendApi.getUserFriends(this.currentUser!.id);
+			this.userGames = await dataApi.getUserGames(this.currentUser!.id);
+			this.userTournaments = await dataApi.getUserTournaments(this.currentUser!.id);
+		} catch (error) {
+			console.error('Erreur lors du chargement du profil:', error);
+			throw error;
+		}
 	}
 
 	/**
 	 * Montage de la page de dashboard.
 	 * 
-	 * Mets à jour les éléments HTML de la page avec des valeurs de démonstration.
-	 * Les éléments HTML suivants sont mis à jour:
-	 * - `#total-games`: le nombre total de parties jouées.
-	 * - `#win-rate`: le taux de victoire.
-	 * - `#avg-score`: le score moyen.
-	 * - `#streak`: le nombre de parties gagnées consécutives.
-	 * 
-	 * Deux graphiques sont également générés:
-	 * - Un graphique de ligne (`#lineChart`) montrant l'évolution
-	 *   du score moyen dans le temps.
-	 * - Un graphique circulaire (`#pieChart`) montrant le nombre de
-	 *   victoires et de défaites.
-	 * 
 	 * @returns {Promise<void>} Une promesse qui se résout lorsque le composant est monté.
 	 */
 	protected async mount(): Promise<void> {
-		// Exemple de stats
-		this.totalGames = 152;
-		this.winRate = 60;
-		this.avgScore = 8.4;
-		this.streak = 5;
+		this.renderStats();
+		this.renderLineChart();
+		this.renderPieChart();
+		// this.defaultGraphics();
+	}
 
-		this.totalGamesElm.textContent = this.totalGames.toString();
-		this.winRateElm.textContent = `${this.winRate}%`;
-		this.avgScoreElm.textContent = this.avgScore.toString();
-		this.streakElm.textContent = this.streak.toString();
+	/**
+	 * Rendu des statistiques utilisateur
+	 */
+	private renderStats(): void {
+		const section = document.getElementById('stats-section') as HTMLDivElement;
+		const template = document.getElementById('stat-card-template') as HTMLTemplateElement;
 
+		if (!section || !template) 
+			return;
+
+		const totalGames = this.userStats!.gamePlayed;
+		const winRate = this.userStats!.winRate;
+
+		const stats = [
+			{ icon: `<i class="fa-solid fa-trophy"></i>`, value: this.userStats!.gameWin || 0, label: 'Average score', translate: "dashboard.averageScore" },
+			{ icon: `<i class="fa-solid fa-circle-xmark"></i>`, value: this.userStats!.gameLoose || 0, label: 'Streaks', translate: "dashboard.streak" },
+			{ icon: `<i class="fa-solid fa-star-half-stroke"></i>`, value: `${winRate}%`, label: 'Win Rate', translate: "profile.winRateLabel" },
+			{ icon: `<i class="fa-solid fa-table-tennis-paddle-ball"></i>`, value: totalGames, label: 'Game played', translate: "profile.gamePlayedLabel" }
+		];
+
+		stats.forEach(stat => {
+			const clone = template.content.cloneNode(true) as DocumentFragment;
+			const value = clone.querySelector('.stat-value') as HTMLElement;
+			const label = clone.querySelector('.stat-label') as HTMLElement;
+			const icon = clone.querySelector('.stat-icon') as HTMLElement;
+
+			icon.innerHTML = stat.icon;
+
+			value.textContent = stat.value.toString();
+			label.textContent = stat.label;
+			label.setAttribute('data-ts', stat.translate);
+
+			section.appendChild(clone);
+			translateService.updateLanguage(undefined, section);
+		});
+	}
+
+	/**
+	 * Rendu du graphique de ligne
+	 */
+	private renderLineChart(): void {
+		const labels = this.userGames.map(game => game.begin);
+		const data = this.userGames.map(game => game.avgScore);
+
+		new Chart(this.lineCtx, {
+			type: 'line',
+			data: {
+				labels: labels,
+				datasets: [{
+					label: 'Average score',
+					data: data,
+					backgroundColor: 'rgba(75, 192, 192, 0.2)',
+					borderColor: 'rgba(75, 192, 192, 1)',
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		});
+	}
+
+	/**
+	 * Rendu du graphique circulaire
+	 */
+	private renderPieChart(): void {
+		const labels = ['Win', 'Loose'];
+		const data = [this.userStats!.gameWin, this.userStats!.gameLoose];
+
+		new Chart(this.pieCtx, {
+			type: 'doughnut',
+			data: {
+				labels: labels,
+				datasets: [{
+					data: data,
+					backgroundColor: [
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(255, 99, 132, 0.2)'
+					],
+					borderColor: [
+						'rgba(75, 192, 192, 1)',
+						'rgba(255, 99, 132, 1)'
+					],
+					borderWidth: 1
+				}]
+			}
+		});
+	}
+
+	/**
+	 * Modèle / exemple de rendu des graphiques utilisateur
+	 */
+	private defaultGraphics(): void {
 		// Scores dans le temps
 		new Chart(this.lineCtx, {
 			type: 'line',
@@ -116,8 +202,4 @@ export class DashboardPage extends BasePage {
 			}
 		});
 	}
-
-	// ===========================================
-	// METHODES PRIVATES
-	// ===========================================
 }
